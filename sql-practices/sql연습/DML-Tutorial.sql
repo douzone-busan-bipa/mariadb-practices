8f12154755fd0f0eaba573b2505e134cf960be09
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-12 14:20:05 +0900

    sql tutorial #1

---
+++ sql-practices/sql연습/DML-Tutorial.sql
@@ -0,0 +1,17 @@
+-- Basic Query
+
+select version(), current_date;
+
+--  대소문 구분이 없다.
+SELECT VERSION(), CURRENT_DATE;
+select vErSion(), current_DATE;
+
+-- 수학함수
+select sin(pi()/4), (4+1)*5;
+
+
+
+
+
+
+

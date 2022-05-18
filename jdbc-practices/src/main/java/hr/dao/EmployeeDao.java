9b8e4851e4eaf289db290e54d31919bfce240d37
더존비즈온 <103369445+douzone-busan-bipa@users.noreply.github.com>
2022-05-18 15:00:25 +0900

    Update EmployeeDao.java

--- jdbc-practices/src/main/java/hr/dao/EmployeeDao.java
+++ jdbc-practices/src/main/java/hr/dao/EmployeeDao.java
@@ -80,4 +80,69 @@ public class EmployeeDao {
 		
 		return result;		
 	}
+	
+	public List<EmployeeVo> findBySalary(int minSalary, int maxSalary) {
+		List<EmployeeVo> result = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			//3. SQL 준비
+			String sql = 
+					"select a.emp_no," +
+					"       a.first_name," +
+					"       a.last_name," +
+					"       b.salary" +
+					"  from employees a, salaries b" +
+					" where a.emp_no = b.emp_no" +
+					"   and b.to_date = '9999-01-01'" +
+					"   and b.salary >= ?" +
+					"   and b.salary <= ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. binding
+			pstmt.setInt(1, minSalary);
+			pstmt.setInt(2, maxSalary);
+			
+			//5. SQL 실행
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				int salary = rs.getInt(4);
+				
+				EmployeeVo vo = new EmployeeVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setSalary(salary);
+				
+				result.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			// clean up
+			try {
+				if(rs != null) {
+					rs.close();
+				}				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;		
+	}	
 }

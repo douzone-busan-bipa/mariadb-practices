5f14297e4169084d8153997a97024542b8eb90e3
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-18 11:21:38 +0900

    jdbc #4

--- jdbc-practices/src/main/java/test/DeleteTest01.java
+++ jdbc-practices/src/main/java/test/DeleteTest01.java
@@ -8,15 +8,49 @@ import java.sql.Statement;
 public class DeleteTest01 {
 
 	public static void main(String[] args) {
-		delete(19L);
-		delete(20L);
+//		delete(19L);
+//		delete(20L);
 //		delete(14L);
-//		delete(15L);
-//		delete(16L);
-//		delete(17L);
-//		delete(18L);
+		
+		delete();
 	}
 
+	private static void delete() {
+		Connection connection = null;
+		Statement stmt = null;
+		
+		try {
+			//1. JDBC Driver 로딩 (JDBC Class 로딩: class loader)
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			//2. 연결하기
+			String url = "jdbc:mysql://192.168.10.55:3306/webdb?charset=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			//3. Statement 생성
+			stmt = connection.createStatement();
+			
+			//4. SQL 실행
+			String sql = "delete from department";
+			stmt.executeUpdate(sql);
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if(stmt != null) {
+					stmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	private static boolean delete(Long no) {
 		boolean result = false;
 		Connection connection = null;
@@ -56,7 +90,4 @@ public class DeleteTest01 {
 		
 		return result;		
 	}
-	
-	
-
 }

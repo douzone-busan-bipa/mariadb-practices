f1eb8176bcd50547668a5ee3b63429d8500ee9ae
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-19 10:16:41 +0900

    bookshop #2

--- jdbc-practices/src/main/java/bookshop/Main.java
+++ jdbc-practices/src/main/java/bookshop/Main.java
@@ -1,10 +1,36 @@
 package bookshop;
 
+import java.util.List;
+import java.util.Scanner;
+
+import bookshop.dao.BookDao;
+import bookshop.vo.BookVo;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		displayBookInfo();
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
+		int num = scanner.nextInt();
+		scanner.close();
+
+//		for (Book book : books) {
+//			if (num == book.getBookNo()) {
+//				book.rent();
+//				break;
+//			}
+//		}
+
+		// System.out.println("*****도서 정보 출력하기******");
+		// displayBookInfo();
+	}
+
+	private static void displayBookInfo() {
+		List<BookVo> list = new BookDao().findAll();
+		for(BookVo vo : list) {
+			System.out.println("[" + vo.getNo() + "] 책 제목:" + vo.getTitle() + ", 작가:" + vo.getAuthorName() + ", 대여 유무:" + vo.getStateCode());
+		}
 	}
 
 }
--- jdbc-practices/src/main/java/bookshop/dao/BookDao.java
+++ jdbc-practices/src/main/java/bookshop/dao/BookDao.java
@@ -18,27 +18,16 @@ public class BookDao {
 		PreparedStatement pstmt = null;
 		
 		try {
-			//1. JDBC Driver 로딩 (JDBC Class 로딩: class loader)
-			Class.forName("org.mariadb.jdbc.Driver");
+			connection = getConnection();
 			
-			//2. 연결하기
-			String url = "jdbc:mysql://192.168.10.55:3306/webdb?charset=utf8";
-			connection = DriverManager.getConnection(url, "webdb", "webdb");
-			
-			//3. SQL 준비
 			String sql = "insert into book values(null, ?, ?, ?)";
 			pstmt = connection.prepareStatement(sql);
-			
-			//4.Mapping(bind)
 			pstmt.setString(1, vo.getTitle());
 			pstmt.setString(2, vo.getStateCode());
 			pstmt.setLong(3, vo.getAuthorNo());
 			
-			//4. SQL 실행
 			int count = pstmt.executeUpdate();
 			result = count == 1;
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} finally {
@@ -64,27 +53,16 @@ public class BookDao {
 		ResultSet rs = null;
 		
 		try {
-			//1. JDBC Driver 로딩 (JDBC Class 로딩: class loader)
-			Class.forName("org.mariadb.jdbc.Driver");
-			
-			//2. 연결하기
-			String url = "jdbc:mysql://192.168.10.55:3306/webdb?charset=utf8";
-			connection = DriverManager.getConnection(url, "webdb", "webdb");
-			
-			//3. SQL 준비
+			connection = getConnection();
+
 			String sql =
 				"   select a.no, a.title, b.name, a.state_code" +
 				"     from book a, author b" +
 				"    where a.author_no = b.no" +
 				" order by no asc";
-			pstmt = connection.prepareStatement(sql);
-			
-			//4. Parameter Mapping
-			
-			//5. SQL 실행
+			pstmt = connection.prepareStatement(sql);			
 			rs = pstmt.executeQuery();
 			
-			//6. 결과처리
 			while(rs.next()) {
 				Long no = rs.getLong(1);
 				String title = rs.getString(2);
@@ -99,8 +77,6 @@ public class BookDao {
 				
 				result.add(vo);
 			}
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} finally {
@@ -120,6 +96,20 @@ public class BookDao {
 		}
 		
 		return result;		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection connection = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://192.168.10.55:3306/webdb?charset=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+		
+		return connection;
 	}
 
 }
